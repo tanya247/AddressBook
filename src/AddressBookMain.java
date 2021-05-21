dff3d17587cc2c62793835abe4a01edb643de60e
user name <user email>
2021-05-21 23:37:34 +0530

    UC10 Showed

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -7,6 +7,8 @@ public class AddressBookMain {
 		System.out.println("Enter 2 : edit contact ");
 		System.out.println("Enter 3 : Delete Contact");
 		System.out.println("Enter 4: Show contact");
+		System.out.println("Enter 5 : Search Contact");
+		System.out.println("Enter 6 : Count Person");
 		int choose = sc.nextInt();
 		switch(choose) {
 		//first name
@@ -16,6 +18,7 @@ public class AddressBookMain {
 			System.out.println("If you want to edit contact directory press 2");
 			System.out.println("If you want to delete contact directory press 3");
 			System.out.println("If you want to search by city or state , press 4");
+			System.out.println("If you want to count by city or state , press 5");
 			int fm = sc.nextInt();
 			if (fm==1) {
 				ac.show();
@@ -29,6 +32,9 @@ public class AddressBookMain {
 			if (fm == 4) {
 				ac.searchContact();
 			}
+			if(fm == 5) {
+				ac.countPerson();
+			}
 			
 			break;
 	
@@ -37,6 +43,7 @@ public class AddressBookMain {
 			System.out.println("If you want to see contact directory press 1");
 			System.out.println("If you want to delete contact directory press 2");
 			System.out.println("If you want to search by city or state , press 3");
+			System.out.println("If you want to count by city or state , press 4");
 			int f = sc.nextInt();
 			if (f==1) {
 				ac.show();
@@ -47,17 +54,24 @@ public class AddressBookMain {
 			if(f==3) {
 				ac.searchContact();
 			}
+			if(f==4) {
+				ac.countPerson();
+			}
 			break;
 		case 3:
 			ac.deleteContact();
 			System.out.println("If you want to see contact directory press 1");
 			System.out.println("If you want to search by city or state , press 2");
+			System.out.println("If you want to count by city or state , press 3");
 			int fo = sc.nextInt();
 			if (fo==1) {
 				ac.show();
 			}
 			if (fo==2) {
 				ac.searchContact();
+			}
+			if(fo == 3) {
+				ac.countPerson();
 			}
 			break;
 				
@@ -67,6 +81,18 @@ public class AddressBookMain {
 			break;
 		case 5:
 			ac.searchContact();
+			System.out.println("If you want to count by city or state , press 1");
+			int fom = sc.nextInt();
+			if(fom == 1) {
+				ac.countPerson();
+			}else {
+				System.out.println("Invalid choice");
+			
+			}
+			break;
+		case 6:
+			ac.countPerson();
+			break;
 		default:
 			System.out.println("Invalid Choice");
 			break;
